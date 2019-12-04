002bbb2f1c7047d85f09e2cd421a5f0d9ef78db3
adityamantri <54004475+adityamantri@users.noreply.github.com>
2019-12-03 21:48:36 -0800

    changed cors
    cors enabled

--- Backend/Backend/app.js
+++ Backend/Backend/app.js
@@ -18,7 +18,7 @@ var cors = require('cors');
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 //Allow Access Control
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://173.193.82.125:31680/');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
